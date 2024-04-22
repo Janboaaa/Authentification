5362e341b59b254eb841eae6f0aea3b9c39d5042
JButzke <jan.butzke@timbertec.com>
2024-04-22 12:36:09 +0200

    add loginRequest, loginEndpoint
    Im Backend Login Basis ergänzt.

---
+++ Backend.WebApi/Data/Requests/loginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.WebApi.Data.Requests
+{
+    public class LoginRequest
+    {
+        public string AuthName { get; set; }
+        public string Key { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ Backend.WebApi/Endpoints/LoginEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using Backend.WebApi.Data.Models;
+using Backend.WebApi.Data.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.WebApi.Endpoints;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly BackendDB _context;
+
+    public UserController(BackendDB context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        User? user = _context.Users.FirstOrDefault(u => u.Name.AuthName == request.AuthName && u.Key == request.Key);
+        if (user == null)
+        {
+            return BadRequest("Invalid authName or key.");
+        }
+        if (!user.RememberMe == request.RememberMe) {}
+
+        return Ok("Login successful.");
+    }
+}
